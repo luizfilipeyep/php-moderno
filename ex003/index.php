e8889353160d422edf6b9dbebc671c75c8d1560b
Luiz Omena <luizfilipeyep@gmail.com>
2024-04-01 10:32:00 -0300

    tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+  <?php 
+    $v = 45.2;
+    var_dump($v);
+
+    $num = (int)"300";
+    var_dump($num);
+  ?>
+</body>
+</html>
