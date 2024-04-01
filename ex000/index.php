85535732f5066f9b89c374ba46ab172c282292c3
Luiz Omena <luizfilipeyep@gmail.com>
2024-04-01 10:10:15 -0300

    ini

--- ex000/index.php
+++ ex000/index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Me livrando da maldição</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Me livrando da maldição</title>
 </head>
 <body>
-    <h1>
-        <?php 
-            echo "Olá, mundo! 🌎 \u{1F30E}";
-            print "Testando";
-        ?>
-    </h1>
-    <p>Vamos tentar nos livrar da maldição</p>
+  <h1>
+    <?php
+      echo "Olá, mundo! 🌎 \u{1F30E}";
+      print "Testando";
+    ?>
+  </h1>
+  <p>Vamos tentar nos livrar da maldição</p>
 </body>
 </html>
